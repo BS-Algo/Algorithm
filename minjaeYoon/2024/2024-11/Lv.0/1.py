5e52be50d9e3c03c825634552c9a04d11b7252e2
stylishy62 <stylishy62@gmail.com>
2024-12-03 08:09:50 +0900

    ✅ 2024.12.03 : 1 sol

--- minjaeYoon/2024/2024-11/Lv.0/1.py
+++ minjaeYoon/2024/2024-11/Lv.0/1.py
@@ -195,3 +195,16 @@ def solution(a, b, flag):
     return a-b
 
 # 22. 코딩 기초 트레이닝 코드 처리하기
+
+# 23. 코딩 기초 트레이닝 
+
+# 24. 코딩 기초 트레이닝 주사위 게임 2
+def solution(a, b, c):
+    answer = 0
+    if a == b and b == c:
+        answer = (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+    elif a == b or b == c or a == c :
+        answer = (a+b+c)*(a**2+b**2+c**2)
+    else:
+        answer = (a+b+c)
+    return answer
