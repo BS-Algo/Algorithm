0d094a32108bbe48651567ac2199ad1a0c504901
GloryMJ <156272655+GloryMJ@users.noreply.github.com>
2024-12-05 23:22:33 +0900

    ✅ 2024/12/05 : 1 sol

--- minjaeYoon/2024/2024-11/Lv.0/1.py
+++ minjaeYoon/2024/2024-11/Lv.0/1.py
@@ -208,3 +208,31 @@ def solution(a, b, c):
     else:
         answer = (a+b+c)
     return answer
+
+# 25. 코딩 기초 트레이닝 원소들의 곱과 합
+def solution(num_list):
+    num = len(num_list)
+    bit, answer, comparison = 0, 1, 0
+    for i in range(num):
+        answer *= num_list[i]
+        comparison += num_list[i]
+        
+    if answer > comparison**2:
+        bit = 0
+    else:
+        bit = 1
+    return bit
+
+num_list = [3, 4, 5, 2, 1]
+num_list_comparison = [5, 7, 8, 3]
+print(solution(num_list))
+print(solution(num_list_comparison))
+
+def solution(num_list):
+    a=1
+    b=0
+    for x in num_list:
+        a*=x
+        b+=x
+    if a<b*b: return 1
+    return 0
--- minjaeYoon/2024/2024-11/Lv.0/prac.py
+++ minjaeYoon/2024/2024-11/Lv.0/prac.py
@@ -1,9 +1,17 @@
-def solution(a, b, c):
-    answer = 0
-    if a == b and b == c:
-        answer = (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
-    elif a == b or b == c or a == c :
-        answer = (a+b+c)*(a**2+b**2+c**2)
+def solution(num_list):
+    num = len(num_list)
+    bit, answer, comparison = 0, 1, 0
+    for i in range(num):
+        answer *= num_list[i]
+        comparison += num_list[i]
+        
+    if answer > comparison**2:
+        bit = 0
     else:
-        answer = (a+b+c)
-    return answer
+        bit = 1
+    return bit
+
+num_list = [3, 4, 5, 2, 1]
+num_list_comparison = [5, 7, 8, 3]
+print(solution(num_list))
+print(solution(num_list_comparison))
