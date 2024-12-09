7e41eb5249584c63f673a49c60424adff47d8a41
GloryMJ <156272655+GloryMJ@users.noreply.github.com>
2024-12-09 20:23:06 +0900

    ✅ 2024-12-09 : 1 sol

--- minjaeYoon/2024/2024-11/Lv.0/1.py
+++ minjaeYoon/2024/2024-11/Lv.0/1.py
@@ -273,3 +273,23 @@ def solution(n, control):
     return n + 10*(control.count('d')-control.count('a')) + (control.count('w')-control.count('s'))
 
 # 29. 코딩 기초 트레이닝 수 조작하기 2
+def solution(numLog):
+    answer = ''
+    for i in range(1, len(numLog)):
+        diff = numLog[i] - numLog[i-1]
+        if diff == 1:
+            answer += 'w'
+        elif diff == -1:
+            answer += 's'
+        elif diff == 10:
+            answer += 'd'
+        elif diff == -10:
+            answer += 'a'
+    return answer
+	
+def solution(log):
+    res=''
+    joystick=dict(zip([1,-1,10,-10],['w','s','d','a']))
+    for i in range(1,len(log)):
+        res+=joystick[log[i]-log[i-1]]
+    return res
--- minjaeYoon/2024/2024-11/Lv.0/prac.py
+++ minjaeYoon/2024/2024-11/Lv.0/prac.py
@@ -1,7 +1,29 @@
 def solution(numLog):
     answer = ''
-
+    for i in range(1, len(numLog)):
+        diff = numLog[i] - numLog[i-1]
+        if diff == 1:
+            answer += 'w'
+        elif diff == -1:
+            answer += 's'
+        elif diff == 10:
+            answer += 'd'
+        elif diff == -10:
+            answer += 'a'
     return answer
+	
+def solution(log):
+    res=''
+    joystick=dict(zip([1,-1,10,-10],['w','s','d','a']))
+    for i in range(1,len(log)):
+        res+=joystick[log[i]-log[i-1]]
+    return res
+
+
+
 
 numLog = [0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1]
+
 print(solution(numLog))
+
+# 1이냐 10이냐로 두고 이후에 크냐 작으냐로 두기
