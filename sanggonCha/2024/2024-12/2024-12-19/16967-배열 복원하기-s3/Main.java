2833739505d79c4e8d24180e3eb2a4dfe95b0d26
차상곤 <yg9618@naver.com>
2024-12-20 18:30:20 +0900

    ✅ 2024-12-19: 1 sol
    2024-12-19: 1 sol

--- sanggonCha/2024/2024-12/2024-12-19/16967-배열 복원하기-s3/Main.java
+++ sanggonCha/2024/2024-12/2024-12-19/16967-배열 복원하기-s3/Main.java
@@ -1,44 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int H = Integer.parseInt(st.nextToken());
-		int W = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
-		int Y = Integer.parseInt(st.nextToken());
-		
-		int BX = H+X, BY = W+Y;
-		int[][] B = new int[BX][BY];
-		for(int i =0; i< BX; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j< BY; j++) {
-				B[i][j]= Integer.parseInt(st.nextToken());
-			}
-		}
-		int[][] A = new int[H][W];
-		for(int i =0; i< BX; i++) {
-			for(int j =0; j< BY; j++) {
-				if(i<X && j< W) {// 위쪽 안겹치는 거 
-					A[i][j] = B[i][j];
-				}else if(j<Y && i<H) { // 왼쪽 안겹치는 거 
-					A[i][j] = B[i][j];
-				}else if(j>=Y && i>=X  && j< W && i<H) {//겹치는 거 
-					A[i][j] = B[i][j]-A[i-X][j-Y];
-				}
-			}
-		}
-		
-		for(int i =0; i< H; i++) {
-			for(int j =0; j< W; j++) {
-				System.out.print(A[i][j]+ " ");
-			}
-			
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        // 첫 줄 변수 입력 받기
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        int[][] origin = new int[H][W];
+        int[][] overlap = new int[H + X][W + Y];
+
+        // 겹쳐진 배열 입력 받기
+        for (int i = 0; i < H + X; i++) {
+            for (int j = 0; j < W + Y; j++) {
+                overlap[i][j] = sc.nextInt();
+            }
+        }
+
+        // 복원 1, 2사분면
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < W; j++) {
+                origin[i][j] = overlap[i][j];
+            }
+        }
+
+        // 복원 3사분면
+        for (int i = X; i < H; i++) {
+            for (int j = 0; j < Y; j++) {
+                origin[i][j] = overlap[i][j];
+            }
+        }
+
+        // 복원 4사분면
+        for (int i = X; i < H; i++) {
+            for (int j = Y; j < W; j++) {
+                origin[i][j] = overlap[i][j] - origin[i - X][j - Y];
+            }
+        }
+
+        // 출력
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W - 1; j++) {
+                System.out.printf("%d ", origin[i][j]);
+            }
+            System.out.printf("%d\n", origin[i][W - 1]);
+        }
+    }
 }
