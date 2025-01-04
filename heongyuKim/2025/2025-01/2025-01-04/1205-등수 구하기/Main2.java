e37a7ba07d512d16addd30e4848880baaea4112d
김헌규 <khg6436@naver.com>
2025-01-04 17:13:16 +0900

    ✅ 2025-01-04 : 1sol
    2025-01-04 : 1sol

---
+++ heongyuKim/2025/2025-01/2025-01-04/1205-등수 구하기/Main2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int newScore = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int[] scoreList = new int[P];
+
+        // 태수의 등수
+        int rank = 1;
+
+        if (N == 0) {
+            System.out.println(1);
+        } else {
+            StringTokenizer scores = new StringTokenizer(br.readLine());
+
+            // 점수들을 랭킹 리스트 수 만큼만 받기
+            for (int i = 0; i < N; i++) {
+                int score = Integer.parseInt(scores.nextToken());
+                scoreList[i] = score;
+            }
+
+            // 랭킹 리스트를 뒤집기
+            Arrays.sort(scoreList);
+
+            if (P == N && scoreList[0] >= newScore) {
+                System.out.println(-1);
+                return;
+            }
+
+            for (int j = P - 1; j >= Math.max(0, P-N-1); j--) {
+                if (scoreList[j] > newScore) {
+                    rank++;
+                } else {
+                    break;
+                }
+            }
+
+            System.out.println(rank);
+
+        }
+
+    }
+}
