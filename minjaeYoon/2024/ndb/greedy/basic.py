a578db6eeb0896dce9247b6fa804ef99dee9f3c5
GloryMJ <stylishy62@gmail.com>
2025-01-14 22:42:55 +0900

    ✅ 2025-01-14 : 1 sol

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -350,3 +350,72 @@ graph = [
 visited = [False] * 9
 
 dfs(graph, 1, visited)
+
+# BFS 복습
+# 탐색 시작 노드 큐에 넣기
+# 큐에서 노드를 꺼내 해당 노드의 인접 노드 중에서 방문하지 않은 노드를 모두 큐에 삽입하고 방문 처리
+# 바로 윗 과정을 끝까지 반복
+
+from collections import deque
+
+def bfs(graph, start, visited):
+    # deque 라이브러리 사용하기
+    queue = deque([start])
+    # 시작 노드 방문 처리
+    visited[start] = True
+    # 큐가 빌 때 까지 반복, 전부 방문 처리하기?
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 노드와 연결되면서 방문 처리 안 된 원소 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+                
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
+
+# 실전 5-3 음료수 얼려 먹기
+n, m = map(int, input().split())
+
+graph = []
+
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs_ice(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    
+    # 미방문 처리
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        
+        # 상하좌우 전부 재귀 처리
+        dfs_ice(x-1, y)
+        dfs_ice(x, y-1)
+        dfs_ice(x+1, y)
+        dfs_ice(x, y+1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs_ice(i, j) == True:
+            result += 1
+            
+print(result)
