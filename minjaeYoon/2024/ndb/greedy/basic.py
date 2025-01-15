3f59a293191820c0f2b2f8083418ce0f2e75a1d8
GloryMJ <stylishy62@gmail.com>
2025-01-16 05:40:01 +0900

    ✅ 2025-01-16 : 1 review

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -450,3 +450,58 @@ def bfs_road(x, y):
     return graph[n -1][m - 1]
 
 print(bfs_road(0, 0))
+
+# 정렬 알고리즘 복습
+# 선택 정렬
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(arr)):
+    min_index = i
+    for j in range(i+1, len(arr)):
+        if arr[min_index] > arr[j]:
+            min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+print(arr)
+
+# 삽입 정렬
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+        else:
+            break
+
+print(arr)
+
+# 퀵 정렬
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(arr, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    
+    while left <= right:
+        while left <= end and arr[left] <= arr[pivot]:
+            left += 1
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
+            
+        if left > right:
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+    
+    quick_sort(arr, start, right-1)
+    quick_sort(arr, right+1, end)
+    
+quick_sort(arr, 0, len(arr)-1)
+print(arr)
+
+# 퀵 정렬 (py)
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
