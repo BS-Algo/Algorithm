5ce8bf01b319b919faba2c5481611b2f0b74c9ea
GloryMJ <stylishy62@gmail.com>
2025-01-20 23:56:51 +0900

    ✅ 2025-01-20 : 1 sol

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -594,4 +594,16 @@ for i in range(k):
     
 print(sum(a))
 
-# 이진 탐색
+# 이진 탐색 - 순차 탐색
+def sequential_search(n, target, arr):
+    for i in range(n):
+        if arr[i] == target:
+            return i + 1
+
+input_data = input().split()
+n = int(input_data[0])
+target = input_data[1]
+
+arr = input().split()
+
+print(sequential_search(n, target, arr))
