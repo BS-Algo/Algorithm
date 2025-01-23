5e8895899e1223f9bd07fdb5a66213d03332b681
GloryMJ <stylishy62@gmail.com>
2025-01-23 20:57:33 +0900

    ✅ 2025-01-23 : 1 sol

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -659,3 +659,95 @@ if result == None:
     
 else:
     print(result + 1)
+    
+# 실전 문제 : 부품 찾기
+def b_search(arr, start, target, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        
+        elif arr[mid] > target:
+            end = mid -1
+            
+        else:
+            start = mid + 1
+    return None
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+m = int(input())
+
+x = list(map(int, input().split()))
+
+for i in x:
+    result = b_search(arr, i, 0, n-1)
+    if result != None:
+        print('Yes', end=' ')
+    else:
+        print('No', end=' ')
+        
+# 계수 정렬로 부품 탐색 풀기
+n = int(input())
+arr = [0] * 1000001
+
+for i in input().split():
+    arr[int(i)] = 1
+    
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if arr[i] == 1:
+        print('yes')
+        
+    else:
+        print('No')
+        
+# 실전 문제 : 떡볶이 떡 만들기
+# 파나메트릭 서치 유형 문제
+n, m = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+start = 0
+end = max(arr)
+
+result = 0
+
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for i in arr:
+        if x > mid:
+            total += x - mid
+    
+    if total < m:
+        end = mid - 1
+        
+    else:
+        result = mid
+        start = mid + 1
+        
+print(result)
+
+# DP (Dynamic Programming)
+# 한 번 계산한 문제는 다시 계산하지 않도록
+
+# 실전문제 : 1로 만들기
+x = int(input().split())
+
+d = [0] * 30001
+
+for i in range(2, x + 1):
+    d[i] = d[i - 1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2]+1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3]+1)
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i//5]+1)
+        
+print(d[x])
+    
