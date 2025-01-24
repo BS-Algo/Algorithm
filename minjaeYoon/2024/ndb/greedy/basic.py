4ea29b6bfe2497360eba6c106850cff23a2f8a12
GloryMJ <stylishy62@gmail.com>
2025-01-24 19:10:40 +0900

    ✅ 2025-01-24 : 1 sol

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -750,4 +750,20 @@ for i in range(2, x + 1):
         d[i] = min(d[i], d[i//5]+1)
         
 print(d[x])
+
+# 실전 문제 : 개미 전사
+n = int(input())
+arr = list(map(int, input().split()))
+
+d = [0] * 100
+
+d[0] = arr[0]
+d[1] = max(arr[0], arr[1])
+
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + arr[i])
+
+print(d[n - 1])
+
+
     
