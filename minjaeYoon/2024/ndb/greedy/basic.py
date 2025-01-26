e8625e70d339f16c0e89318d32516e1bd32c51f6
GloryMJ <stylishy62@gmail.com>
2025-01-26 23:57:01 +0900

    ✅ 2025-01-26 : 1 sol

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -765,5 +765,18 @@ for i in range(2, n):
 
 print(d[n - 1])
 
+# 실전 문제 : 바닥 공사
+n = int(input())
+
+d = [0] * 1001
+
+d[1] = 1
+d[2] = 3
+
+for i in range(3, n+1):
+    d[i] = (d[i-1]+2 * d[i-2]) % 796796
+    
+print(d[n])
+
 
     
