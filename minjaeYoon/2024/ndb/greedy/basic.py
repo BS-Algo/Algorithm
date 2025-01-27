38a29c87cf65b80056ed2317ce414c30eac566fc
GloryMJ <stylishy62@gmail.com>
2025-01-27 18:55:49 +0900

    ✅ 2024-01-27 : 1 sol

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -778,5 +778,29 @@ for i in range(3, n+1):
     
 print(d[n])
 
+# 실전 문제 : 효율적인 화폐 구성
+n, m = map(int, input().split())
 
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
     
+d = [10001] * (m+1)
+
+d[0] = 0
+
+for i in range(n):
+    for j in range(arr[i], m+1):
+        if d[j - arr[i]] != 10001:
+            d[j] = min(d[j], d[j - arr[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
+    
+    
+#----------------------------------------------
+# ch9. 최단 경로
+# 다익스트라 최단 경로
