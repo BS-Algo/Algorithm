808df6322c22e61d11ae46709e399c4867b75072
GloryMJ <stylishy62@gmail.com>
2025-02-08 22:18:52 +0900

    ✅ 2025-02-08 : 1 sol

--- minjaeYoon/2024/ndb/greedy/basic.py
+++ minjaeYoon/2024/ndb/greedy/basic.py
@@ -1070,3 +1070,40 @@ if cycle:
     print("O")
 else:
     print("X")
+    
+
+# 신장 트리 - 크루스칼 알고리즘
+def find_parent3(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent3(parent, parent[x])
+    return parent[x]
+
+def union_parent3(parent, a, b):
+    a = find_parent3(parent, a)
+    b = find_parent3(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+
+edges = []
+result = 0
+
+for i in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+    
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent3(parent, a) != find_parent3(parent, b):
+        union_parent3(parent, a, b)
+        result += cost
+        
+print(result)
+
+    
