e74717440092051e3d308e97f5e7c27bd6bbbc00
EunseopKim <subway9852@gmail.com>
2025-02-11 23:24:22 +0900

    ✅ 2025-02-11 : 1 sol

---
+++ eunseopKim/2025/2025-02/26057.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int l, t; 
+  cin >> l >> t;
+  int dif = 2*t - l;
+  cout << dif << "\n";
+
+  return 0;
+}
