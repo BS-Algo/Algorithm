27cd0826068553239ae3028628aa83261b61e37f
GloryMJ <stylishy62@gmail.com>
2025-02-21 23:59:06 +0900

    ✅ 2025-02-21 : 1 sol

--- minjaeYoon/2025/programmers/Lv.0.py
+++ minjaeYoon/2025/programmers/Lv.0.py
@@ -68,3 +68,18 @@ while 70 <= money < 100:
     month += 1
 
 print(month)
+
+# [PCCE 기출문제] 5번 / 심폐소생술
+
+def solution(cpr):
+    answer = []
+    basic_order = ["check", "call", "pressure", "respiration", "repeat"]
+    for action in basic_order:
+        for i in range(len(basic_order)):
+            if action == basic_order[i]:
+                answer.append(cpr.index(action)+1)
+    return answer
+
+cpr = ["call", "respiration", "repeat", "check", "pressure"]
+
+print(solution(cpr))
