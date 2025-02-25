c6c71e81ce365b28747f175a2951417e92164cb4
sanggonCha <yg9618@naver.com>
2025-02-25 14:36:40 +0900

    ✅ 2025-02-25: 4 sol
    2025-02-25: 4 sol

--- sanggonCha/2025/2025-02/2025-02-25/DP-2193-이친수-s3/Main.java
+++ sanggonCha/2025/2025-02/2025-02-25/DP-2193-이친수-s3/Main.java
@@ -1,30 +1,32 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    private static int N;
     private static long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // N 입력 받기
-        N = Integer.parseInt(br.readLine());
+        // T 입력 받기
+        int T = Integer.parseInt(br.readLine());
 
         // dp
-        if (N == 1) {
-            System.out.println(1);
-        } else if (N == 2) {
-            System.out.println(1);
-        } else {
-            dp = new long[N + 1];
-            dp[1] = 1;
-            dp[2] = 1;
-            for (int i = 3; i <= N; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
-
-            System.out.println(dp[N]);
+        dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
         }
+
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
