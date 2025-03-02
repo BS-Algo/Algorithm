bd8bec81f4b3e9e768468931520ecd19def4e444
alstn0575 <alstn0575@naver.com>
2025-03-02 23:49:55 +0900

    ✅ 2025-03-02 : 1 sol
    2025-03-02 : 1 sol

---
+++ minsooKim/2025/2025-03/2025-03-02/1065.py
@@ -0,0 +1,10 @@
+N = int(input())
+hansu = 0
+for i in range(1, N + 1):
+    num_lst = list(map(int, str(i)))
+    if i < 100:
+        hansu += 1
+    elif num_lst[0] - num_lst[1] == num_lst[1]-num_lst[2]:
+        hansu +=1
+    print(num_lst)
+print(hansu)
