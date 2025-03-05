6dfb2ebd3c80d7a3b25ef619d35630a92e5cfacb
pjy980526@naver.com <pjy980526@naver.com>
2025-03-06 00:35:11 +0900

    ✅ 2025-03-06 : 1sol
    2025-03-06 : 1sol

---
+++ jaeyeongPark/2025/2025-03/baekjoon/1065.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// 한수인지 판별하는 함수
+bool isHan(int num) {
+    if (num < 100) return true; // 1~99는 모두 한수
+    
+    int a = num / 100;         // 백의 자리
+    int b = (num / 10) % 10;   // 십의 자리
+    int c = num % 10;          // 일의 자리
+    
+    return (a - b) == (b - c); // 등차수열 여부 판별
+}
+
+int main() {
+    int N, count = 0;
