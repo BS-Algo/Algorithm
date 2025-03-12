cd042d15a6da53785176fb574da80b8bf80396de
alstn0575 <alstn0575@naver.com>
2025-03-12 22:49:29 +0900

    ✅ 2025-03-12 : 1 sol
    2025-03-12 : 1 sol

---
+++ minsooKim/2025/2025-03/2025-03-12/11501.py
@@ -0,0 +1,16 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    lst = arr[::-1]
+    print(lst)
+    cnt = 0
+    max_num = lst[0]
+    for i in range(1, N):
+        if lst[i] > max_num:
+            max_num = lst[i]
+        else:
+            cnt += max_num - lst[i]
+    print(cnt)
+        
+
