54d1f8ac2d926289d5ada6a6d9020243e7db2d2f
GloryMJ <stylishy62@gmail.com>
2025-04-08 22:58:46 +0900

    ✅ 2025-04-08 : 1 sol

--- minjaeYoon/2025/2025-04/prac.py
+++ minjaeYoon/2025/2025-04/prac.py
@@ -1,21 +1,13 @@
-# 2의 영역
-def solution(arr):
-    answer = []
-    indices = []
-    
-    for i in range(len(arr)):
-        if arr[i] == 2:
-            indices.append(i)
-    
-    if not indices:
-        return [-1]
-    
-    if len(indices) == 1:
-        return [2]
-    
-    answer += arr[indices[0]:indices[-1]+1]
-    
-    return answer
+# 배열 조각하기
+def solution(arr, query):
+    for i in range(len(query)):
+        if i % 2 == 0:
+            arr = arr[:query[i]+1]
+        elif i % 2 == 1:
+            arr = arr[query[i]:]
+    return arr
 
-arr = [1, 2, 1, 4, 5, 2, 9]	
-print(solution(arr))
+arr = [0, 1, 2, 3, 4, 5]
+query = [4, 1, 2]
+
+print(solution(arr, query))
