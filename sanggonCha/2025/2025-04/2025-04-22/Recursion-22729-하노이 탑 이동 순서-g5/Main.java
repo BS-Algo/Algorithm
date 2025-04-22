74441eb32998bd56fbe0c3075627b79c82c8a0fd
sanggonCha <yg9618@naver.com>
2025-04-22 18:48:12 +0900

    ✅ 2025-04-22: 1 sol
    2025-04-22: 1 sol

--- sanggonCha/2025/2025-04/2025-04-22/Recursion-22729-하노이 탑 이동 순서-g5/Main.java
+++ sanggonCha/2025/2025-04/2025-04-22/Recursion-22729-하노이 탑 이동 순서-g5/Main.java
@@ -14,12 +14,25 @@ class Main {
         System.out.println(sb);
     }
 
-    private static void hanoi(int N, int from, int to, int sub) {
-        if (N == 0) return; // 더 이상 옮길 원판이 없으면 종료
+    // N = 3 이라고 생각
+    private static void hanoi(int N, int from, int to, int sub) { // hanoi(3, 1, 3, 2)
+        if (N == 0) return; // 더 이상 옮길 원판이 없으면 종료 - N = 0일 때 종료
 
-        hanoi(N - 1, from, sub, to); // N-1개를 from에서 sub로 올기기
-        sb.append(from).append(' ').append(to).append('\n'); // N번째 원판 이동 기록
-        count++;
-        hanoi(N - 1, sub, to, from); // N-1개를 sub에서 to로 옮기기
+        // 1. N-1개를 from에서 sub로 올기기
+        hanoi(N - 1, from, sub, to); 
+        // hanoi(2, 1, 2, 3)
+        // hanoi(1, 1, 3, 2)
+
+        // 2. N번째 원판 이동 기록
+        sb.append(from).append(' ').append(to).append('\n');
+        // 1 3 기록: 크기 1의 원판이 1에서 3으로 이동
+        
+        // 이동횟수 추가
+        count++; 
+
+        // 3. N-1개를 sub에서 to로 옮기기
+        hanoi(N - 1, sub, to, from); 
+        // hanoi(2, 2, 3, 1) - 2
+
     }
 }
