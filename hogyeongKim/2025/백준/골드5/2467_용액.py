5894be71dacba035d9b4fa1e51f842683e19058d
김호경 <ssafy1123992@gmail.com>
2025-04-27 23:17:32 +0900

    ✅ 2025-4-27 : 1 sol
    2025-4-27 : 1 sol

---
+++ hogyeongKim/2025/백준/골드5/2467_용액.py
@@ -0,0 +1,24 @@
+import sys
+n = int(input())
+
+solutions = list(map(int, input().split()))
+
+mn_solution = sys.maxsize
+result = []
+left, right = 0, n-1
+cur_left, cur_right = 0, n-1
+while left < right:
+    new_solution = solutions[left] + solutions[right]
+    if new_solution == 0:
+        cur_left, cur_right = left, right
+        break
+    else:
+        if mn_solution > abs(new_solution):
+            mn_solution = abs(new_solution)
+            cur_left, cur_right = left, right
+        
+        if new_solution > 0:
+            right -= 1
+        else:
+            left += 1
+print(solutions[cur_left], solutions[cur_right])
