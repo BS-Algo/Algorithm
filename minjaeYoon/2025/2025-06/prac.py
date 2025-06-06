b1a8ba7be7ca84a4ccfd3cce7f1604b56fb0260e
GloryMJ <stylishy62@gmail.com>
2025-06-06 23:20:10 +0900

    ✅ 2025-06-06 : 1 sol

--- minjaeYoon/2025/2025-06/prac.py
+++ minjaeYoon/2025/2025-06/prac.py
@@ -1,46 +1,19 @@
-# 옹알이 (1)
-def solution(babbling):
+# 삼각형의 완성조건 (2)
+def solution(sides):
     answer = 0
-    able = ["aya", "ye", "woo", "ma"]
-    
-    for bab in babbling:
-        # 불가능 조건
-        if len(bab) > 10 or len(bab) < 2 or len(bab) == 9:
-            answer += 0
-        # 단어만 일치
-        elif bab in able:
+    # 조건 1 : 가장 긴 변이 sides 안에 있을 경우
+    maxi = max(sides)
+    mini = min(sides)
+    sums = sum(sides)
+    for i in range(maxi): # 자연적으로 -1까지 되니까
+        if i + mini > maxi:
             answer += 1
-        # 조합 따지기 (2단어) => 4, 5, 6
-        elif 4 <= len(bab) <= 6:
-            for i in range(4):
-                for j in range(4):
-                    if i != j:
-                        sentence = able[i] + able[j]
-                        if bab == sentence:
-                            answer += 1
-        # 조합 따지기 (3단어)
-        elif 7 <= len(bab) <= 8:
-            for i in range(4):
-                for j in range(4):
-                    if i != j:
-                        for k in range(4):
-                            if k != i and k != j :
-                                sentence = able[i] + able[j] + able[k]
-                                if bab == sentence:
-                                    answer += 1
-        # 조합 따지기 (4단어)
-        elif len(bab) == 10:
-            for i in range(4):
-                for j in range(4):
-                    for k in range(4):
-                        for l in range(4):
-                            if l != i and l != j and l != k:
-                                sentence = able[i] + able[j] + able[k] + able[l]
-                                if bab == sentence:
-                                    answer += 1 
-        
+    # 조건 2 : 가장 긴 변을 찾는 경우
+    for i in range(sums):
+        if i < sums and i >= maxi:
+            answer += 1
     return answer
 
-babbling = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]	
+sides = [3, 6]
 
-print(solution(babbling))
+print(solution(sides))
