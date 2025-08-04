ef7f60d1453ef8b08560a2957707e975fc37d326
GloryMJ <stylishy62@gmail.com>
2025-08-04 19:00:46 +0900

    ✅ 2025-08-04 : 1 sol

---
+++ minjaeYoon/2025/2025-08/0804.py
@@ -0,0 +1,18 @@
+# 코드 처리하기
+def solution(code):
+    answer = ''
+    mode = 0
+    
+    for idx in range(len(code)):
+        if code[idx] == "1":
+            mode = 1 - mode
+        else:
+            if mode == 0 and idx % 2 == 0:
+                answer += code[idx]
+            elif mode == 1 and idx % 2 == 1:
+                answer += code[idx]
+    
+    
+    return answer if answer else "EMPTY"
+
+print(solution('abc1abc1abc'))
