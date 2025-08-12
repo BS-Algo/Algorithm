b55407586ace5cb4df6ecac394bdb7c47ff90a28
sanggonCha <yg9618@naver.com>
2025-08-12 22:37:57 +0900

    ✅ 2025-08-12: 1 sol
    check

--- sanggonCha/2025/2025-08/2025-08-11/중성화 여부 파악하기/Solution.sql
+++ sanggonCha/2025/2025-08/2025-08-11/중성화 여부 파악하기/Solution.sql
@@ -1,11 +1,7 @@
 SELECT
     ANIMAL_ID,
     NAME,
-    CASE
-        WHEN SEX_UPON_INTAKE LIKE '%Neutered%'
-        OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
-        ELSE 'X'
-    END AS '중성화'
+    DATE_FORMAT (DATETIME, '%Y-%m-%d') AS 날짜
 FROM
     ANIMAL_INS
 ORDER BY
