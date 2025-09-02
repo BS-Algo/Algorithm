ad3cab10b08207b585f037576dd925cb29060de8
GloryMJ <stylishy62@gmail.com>
2025-09-02 19:24:25 +0900

    ✅ 2025-09-02 : 1 sol

---
+++ minjaeYoon/2025/2025-09/0902.py
@@ -0,0 +1,10 @@
+# 가운데 숫자 가져오기
+def solution(s):
+    answer = ''
+    if len(s) % 2 == 0:
+        answer += s[len(s) // 2 -1 : len(s) // 2 +1]
+    else:
+        answer += s[len(s) // 2]
+    return answer
+
+print(solution("qwer"))
