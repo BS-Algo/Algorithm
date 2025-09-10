4128b91192d4c5f120f44257565653c6d612766e
GloryMJ <stylishy62@gmail.com>
2025-09-10 19:08:45 +0900

    ✅ 2025-- : 1 sol

---
+++ minjaeYoon/2025/2025-09/0910.py
@@ -0,0 +1,13 @@
+# 행렬의 덧셈
+# https://school.programmers.co.kr/learn/courses/30/lessons/12950
+def solution(arr1, arr2):
+    answer = [[0]*len(arr1[0]) for _ in range(len(arr1))]
+    
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+    
+    return answer
+
+print(solution([[1,2],[2,3]], [[3,4],[5,6]]))
+print(solution([[1],[2]], [[3],[4]]))
