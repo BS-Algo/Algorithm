052a638ee23d77e87c35906d8033d3a4946b878c
GloryMJ <stylishy62@gmail.com>
2025-09-16 16:51:07 +0900

    ✅ 2025-09-16 : 1 sol

---
+++ minjaeYoon/2025/2025-09/0916.py
@@ -0,0 +1,10 @@
+# 크기가 작은 부분 문자열
+# https://school.programmers.co.kr/learn/courses/30/lessons/147355
+def solution(t, p):
+    answer = 0
+    for i in range(len(t)-len(p)+1):
+        if int(t[i:i+len(p)]) <= int(p):
+            answer += 1
+    return answer
+
+print(solution("10203", "15"))
