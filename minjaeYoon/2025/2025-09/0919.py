a547fe244e9304e64f948a9211623105e88bcb62
GloryMJ <stylishy62@gmail.com>
2025-09-19 21:17:57 +0900

    ✅ 2025-09-19 : 1 sol

---
+++ minjaeYoon/2025/2025-09/0919.py
@@ -0,0 +1,7 @@
+# 문자열 내 마음대로 정렬하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12915
+def solution(strings, n):
+    strings = sorted(strings, key=lambda x: (x[n], x))
+    return strings
+
+print(solution(["sun", "bed", "car"], 1))
