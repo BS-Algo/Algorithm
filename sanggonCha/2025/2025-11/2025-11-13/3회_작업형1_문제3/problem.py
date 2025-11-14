1c17665011d50044fe9cd412b60e9abfab946f7c
sanggonCha <yg9618@naver.com>
2025-11-14 23:58:18 +0900

    ✅ 2025-11-14: 1 sol
    2025-11-14: 1 sol

--- sanggonCha/2025/2025-11/2025-11-13/3회_작업형1_문제3/problem.py
+++ sanggonCha/2025/2025-11/2025-11-13/3회_작업형1_문제3/problem.py
@@ -1,32 +1,50 @@
-#답:
+# 정답:
 
-import pandas as pd
+# 1. 데이터 불러오기
+X_train = pd.read_csv('train_data.csv')
+y_train = pd.read_csv('y_train.csv')
+X_test = pd.read_csv('test_data.csv')
+# X_train
+# y_train
+# 2.'index' 컬럼 제거
+X_train2 = X_train.drop(columns=['index'])
+X_test2 = X_test.drop(columns=['index'])
+X_test2
+ 
+# 3. 문자형 컬럼을 숫자로 변환합니다.
+x_train3 = pd.get_dummies(X_train2)
+x_test3 = pd.get_dummies(X_test2)
 
-# 1.CSV 파일에서 데이터 로드
-df = pd.read_csv('employee_data.csv')
-# df
 
-# 2.전체 데이터 행수
-len(df) # 200
+# 4. 데이터를 정규화합니다.
+from sklearn.preprocessing import MinMaxScaler
 
-# 3.각 컬럼의 결측값 비율 계산
-# df.isnull().sum()
-# Employee_ID	0
-# Age	80
-# Job_Level	100
-# Salary	176
-# Department	45
+scaler = MinMaxScaler()
+scaler.fit(x_train3)
+x_train4 = scaler.transform(x_train3)
+# x_train4
 
-# df.isnull().sum() / len(df)
-# Employee_ID	0.000
-# Age	0.080
-# Job_Level	0.100
-# Salary	0.176
-# Department	0.045
+# scaler.fit(x_test3)
+x_test4 = scaler.transform(x_test3)
+x_test4
 
-nan_ratio = df.isnull().sum() / len(df)
-# nan_ratio
+# 5. 모델 생성
+from sklearn.ensemble import RandomForestClassifier
 
-# 4. 결측값 비율이 가장 높은 컬럼명 출력
-max_column = nan_ratio.idxmax() # idxmax: index of maximum
-print(max_column)
+model = RandomForestClassifier()
+
+# 6. 모델 훈련
+model.fit(x_train4, y_train)
+
+# 7. 모델 예측
+pred = model.predict(x_test4)
+pred
+
+# 8. 정답 제출
+pd.DataFrame({'index': X_test['index'], 'label': pred}).to_csv('003000.csv', index=False)
+
+# 9. 모델 평가
+from sklearn.metrics import accuracy_score
+
+y_hat = model.predict(x_train4)
+accuracy_score(y_hat, y_train)
