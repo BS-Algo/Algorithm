c031c96b904ddbbc4adccb772f72671fca66baab
sanggonCha <yg9618@naver.com>
2026-05-20 21:54:57 +0900

    ✅ 2026-05-20: 1 sol
    2026-05-20: 1 sol

---
+++ sanggonCha/2026/2026-05/2026-05-20/3회 작업형 2-1/problem.py
@@ -0,0 +1,43 @@
+# 정답:
+
+# 1. 데이터 불러오기
+X_train = pd.read_csv('train_data.csv') # 훈련 데이터
+y_train = pd.read_csv('y_train.csv') # 정답 데이터
+X_test = pd.read_csv('test_data.csv') # 테스트 데이터
+
+# 2.'index' 컬럼 제거
+x_train2 = X_train.drop(columns='index')
+x_test2 = X_test.drop(columns='index')
+
+# 3. 문자형 컬럼을 숫자로 변환합니다.
+x_train3 = pd.get_dummies(x_train2)
+x_test3 = pd.get_dummies(x_test2)
+
+# 4. 데이터를 정규화합니다.
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+
+scaler.fit(x_train3)
+x_train4 = scaler.transform(x_train3)
+
+scaler.fit(x_test3)
+x_test4 = scaler.transform(x_test3)
+
+# 5. 모델 생성
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
+
+# 6. 모델 훈련
+model.fit(x_train4, y_train)
+
+# 7. 모델 예측
+pred = model.predict(x_test4)
+
+# 8. 정답 제출
+pd.DataFrame({'index': X_test['index'], 'label': pred}).to_csv('003000.csv', index=False)
+
+# 9. 모델 평가
+from sklearn.metrics import accuracy_score
+y_hat = model.predict(x_train4)
+accuracy_score(y_hat, y_train)
