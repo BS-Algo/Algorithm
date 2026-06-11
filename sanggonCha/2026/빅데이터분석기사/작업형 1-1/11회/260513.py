bd8790e7d60746a0aea9f67feba7d705bb84fc7d
sanggonCha <yg9618@naver.com>
2026-06-11 23:49:20 +0900

    ✅ 2026-06-11: 3 sol
    2026-06-11: 3 sol

--- sanggonCha/2026/빅데이터분석기사/작업형 1-1/11회/260513.py
+++ sanggonCha/2026/빅데이터분석기사/작업형 1-1/11회/260513.py
@@ -5,18 +5,24 @@ import pandas as pd
 # 데이터 읽기
 df = pd.read_csv("data_usage.csv")
 df.head(30)
-#df.Country.unique() # 7개 국가
 
-# 1.주어진 데이터셋에서 연도(Year)별로 가스 사용량(Gas_Usage) 이 가장 많은 행들을 골라냅니다.
-idx = df.groupby('Year')['Gas_Usage'].idxmax()
-# idx
-a = df.iloc[idx, ]
+# 1. 가스 사용량(Gas_Usage) 컬럼의 모든 값에 대해 제곱근(Square Root) 값을 계산하여
+#    새로운 변수로 정의한다.
+import numpy as np
+df['sqrt_Gas_Usage2'] = np.sqrt(df['Gas_Usage'])
+
+df['sqrt_Gas_Usage'] = df['Gas_Usage'] ** 0.5
+df
+
+# 2. 2020년 데이터만을 필터링한다.
+a = df.loc[df['Year'] == 2020 , :]
 a
 
-# 2.골라낸 행들중에서 가장 많이 등장한 국가(최빈 국가) 를 찾는다.
-b = a['Country'].value_counts().reset_index()
+# 3. 필터링된 데이터에 대해 1번 과정에서 생성한 제곱근 가스 사용량의 평균(Mean) 을 계산하시오.
+m = a['sqrt_Gas_Usage'].mean()
+m
 
-# 3.가장 많이 등장한 국가가 몇번 출현했는지 등장한 횟수를 출력하시오
-c = b.loc[0, ['count']]
-ans = c[0] 
+# 4. 최종 결과값은 소수점 셋째 자리에서 반올림하여 소수점 둘째 자리까지
+#    출력하시오. (예: 12.345 -> 12.35)
+ans = round(m, 2)
 print(ans)
