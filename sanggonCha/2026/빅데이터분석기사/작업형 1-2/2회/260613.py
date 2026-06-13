3ce8c7bc095a8ef0462a2affe4ef6e005a0fa201
sanggonCha <yg9618@naver.com>
2026-06-13 19:03:19 +0900

    ✅ 2026-06-13: 5 sol
    2026-06-13: 5 sol

---
+++ sanggonCha/2026/빅데이터분석기사/작업형 1-2/2회/260613.py
@@ -0,0 +1,23 @@
+#답:
+#1. 데이터 불러오기
+df = pd.read_csv("users.csv")
+#df.isnull().sum()
+
+#2. 데이터셋 앞에서부터 75% 데이터만 선택하기
+df2 = df.head(int(len(df) * 0.75))
+
+#3. 'feature' 컬럼의 결측치 처리전 표준편차 값 구하기 (표본)
+before_std = df2['feature'].std()
+
+#4. 'feature' 컬럼의 최빈값을 구합니다.
+d2_fea_mode = df2['feature'].mode()[0]
+
+#5. 'feature' 컬럼의 결측치를 최빈값으로 채웁니다.
+df2['feature'] = df2['feature'].fillna(d2_fea_mode)
+
+#6. 'feature' 컬럼의 결측치 처리후 표준편차 값 구하기
+after_std = df2['feature'].std(ddof=1)
+
+#7. 두 표준편차의 차이의 절대값 계산하기
+diff = abs(before_std - after_std)
+print(diff)
