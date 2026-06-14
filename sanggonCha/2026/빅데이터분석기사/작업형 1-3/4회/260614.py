051fd3f0f3db9a137dffaa18b93dc9ad598c494e
sanggonCha <yg9618@naver.com>
2026-06-14 18:49:53 +0900

    ✅ 2026-06-14: 9 sol
    2026-06-14: 9 sol

---
+++ sanggonCha/2026/빅데이터분석기사/작업형 1-3/4회/260614.py
@@ -0,0 +1,13 @@
+import pandas  as  pd
+
+#1. 데이터 불러오기
+df = pd.read_csv("employee_data.csv")
+
+#2. date_hired 를 날짜형 변환
+# df.info() # date_hired: object
+df['date_hired'] = pd.to_datetime(df['date_hired'])
+# df.info() # date_hired: datetime64
+
+#3. date_hired가 2020년 1월 1일 이후에 입사했으며, country가 'United States'인  직원수 출력
+ans = len(df.loc[(df['date_hired'] > '2020-01-01') & (df['country'] == 'United States'), :])
+print(ans)
