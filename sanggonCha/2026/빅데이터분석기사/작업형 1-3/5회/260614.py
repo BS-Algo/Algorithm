71ec79e8326fcc1abcca7f62d9e9737140f60438
sanggonCha <yg9618@naver.com>
2026-06-14 18:59:52 +0900

    ✅ 2026-06-14: 12 sol
    2026-06-14: 12 sol

---
+++ sanggonCha/2026/빅데이터분석기사/작업형 1-3/5회/260614.py
@@ -0,0 +1,14 @@
+#답:
+import pandas as pd
+
+# CSV 파일에서 데이터 로드
+df = pd.read_csv('car_production_data.csv')
+
+#자동차 제조 회사의 각 공장에서 생산된 차량 수와 이송된 차량 수를 바탕으로,
+# 순수 생산량(생산된 차량 수 - 이송된 차량 수)
+df['Natural'] = df['Produced'] - df['Transferred']
+
+# 순수 생산량(생산된 차량 수 - 이송된 차량 수)이 가장 많은 공장의 생산된 차량수를 구하세요.
+max_idx = df['Natural'].idxmax()
+ans = df.iloc[max_idx, 1] # 1번 컬럼이 Produced
+print(ans)
